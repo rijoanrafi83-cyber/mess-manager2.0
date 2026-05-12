bd30f633ab7bbe0816bd27314d949ec30b32b8af
rijoanrafi83-cyber <rijoanrafi83@gmail.com>
2026-05-12 22:44:56 +0600

    final firebase config fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPP07U5ubZfHAeSUuUSWHfoSrLPI33Ahc",
+  apiKey: "AIzaSyAPP07U5UbZfHAeSUuUSWHfoSrLPI33Ahc",
   authDomain: "mess-manager-2.firebaseapp.com",
   projectId: "mess-manager-2",
- storageBucket: "mess-manager-2.appspot.com",
+  storageBucket: "mess-manager-2.firebasestorage.app",
   messagingSenderId: "13664139775",
   appId: "1:13664139775:web:3a48a7e9718b53dc28cfca",
+  measurementId: "G-FYZ4ZYGZFX"
 };
 
 const app = initializeApp(firebaseConfig);
