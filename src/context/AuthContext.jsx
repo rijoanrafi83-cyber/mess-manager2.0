d87d3400051417c91fd3e3e916a7916b30b6bb29
rijoanrafi83-cyber <rijoanrafi83@gmail.com>
2026-05-12 23:12:36 +0600

    fix firebase config

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -9,7 +9,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
-import { auth, db } from "../firebase";
+import { auth, db } from "../firebase/firebase";
 
 const AuthContext = createContext(null);
 
