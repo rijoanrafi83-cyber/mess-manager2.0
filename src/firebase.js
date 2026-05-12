4fd1f46a270a34c413f72185da118dc3fba41d88
rijoanrafi83-cyber <rijoanrafi83@gmail.com>
2026-05-12 21:59:27 +0600

    fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAPPO7U5ubZfHAeSUuUSWHfoSrLPI33Ahc",
+  apiKey: "AIzaSyAPP07U5ubZfHAeSUuUSWHfoSrLPI33Ahc",
   authDomain: "mess-manager-2.firebaseapp.com",
   projectId: "mess-manager-2",
   storageBucket: "mess-manager-2.firebasestorage.app",
   messagingSenderId: "13664139775",
   appId: "1:13664139775:web:3a48a7e9718b53dc28cfca",
-  measurementId: "G-FYZ4ZYGZFX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
