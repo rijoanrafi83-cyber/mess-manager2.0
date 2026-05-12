ffcb6495a79f46b5b3c6ac94f74411a4ca797299
rijoanrafi83-cyber <rijoanrafi83@gmail.com>
2026-05-12 22:12:58 +0600

    fix storage bucket

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAPP07U5ubZfHAeSUuUSWHfoSrLPI33Ahc",
   authDomain: "mess-manager-2.firebaseapp.com",
   projectId: "mess-manager-2",
-  storageBucket: "mess-manager-2.firebasestorage.app",
+  storageBucket: "mess-manager-2.appspot.com"
   messagingSenderId: "13664139775",
   appId: "1:13664139775:web:3a48a7e9718b53dc28cfca",
 };
